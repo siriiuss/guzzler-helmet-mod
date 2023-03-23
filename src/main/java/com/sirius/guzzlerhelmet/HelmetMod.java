70230377942643e8eddd371e31481523a7104128
siriiuss <ahmetdur10@gmail.com>
2023-03-23 14:27:04 +0300

    Tags added Recipes added Loot tables added but not used

--- src/main/java/com/sirius/guzzlerhelmet/HelmetMod.java
+++ src/main/java/com/sirius/guzzlerhelmet/HelmetMod.java
@@ -16,6 +16,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
+
+
 @Mod(HelmetMod.MOD_ID)
 public class HelmetMod
 {
